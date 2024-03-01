650d16eb2358efdfdf39b2add80d7f0dbe35f9a0
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-01 17:27:19 +0800

    feat: 新增好友发起聊天功能

---
+++ src/pages/User/components/UserProfile.tsx
@@ -0,0 +1,126 @@
+import { PhoneOutlined, UserOutlined } from '@ant-design/icons';
+import { faComment } from '@fortawesome/free-regular-svg-icons';
+import { faVideo } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Avatar, Divider } from 'antd';
+import PropTypes from 'prop-types'; // 这个库用于定义组件的props的类型
+import './UserProfile.less';
+
+// @ts-ignore
+const UserProfile = ({ avatarUrl, userName, status, onCall, onMessage, onVideo }) => {
+  // @ts-ignore
+  return (
+    <div className="user-profile">
+      <div>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <Avatar shape="square" size={64} src={avatarUrl} />
+          </div>
+          <span style={{ paddingLeft: 20 }}>
+            <div>
+              {userName}
+              <UserOutlined style={{ color: '#1760e7' }} />
+            </div>
+            <div style={{ color: '#9E9E9E' }}>微信号：{userName}</div>
+            <div style={{ color: '#9E9E9E' }}>地区：{userName}</div>
+          </span>
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <div style={{ color: '#9E9E9E' }}>
+              状态
+              <span
+                style={{ paddingLeft: 20 }}
+                className={`user-status ${status === 'online' ? 'online' : 'offline'}`}
+              >
+                {status === 'online' ? '在线' : '离线'}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <div style={{ color: '#9E9E9E' }}>
+              <div style={{ color: '#9E9E9E' }}>
+                备注<span style={{ paddingLeft: 20 }}>点击添加备注</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <div style={{ color: '#9E9E9E' }}>
+              <div style={{ color: '#9E9E9E' }}>
+                个性签名<span style={{ paddingLeft: 20 }}>哈哈哈哈我是签名</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ textAlign: 'center', cursor: 'pointer' }}>
+            <FontAwesomeIcon
+              icon={faComment}
+              style={{
+                fontSize: 25,
+              }}
+            />
+            <div>发消息</div>
+          </div>
+          <div style={{ paddingLeft: 50, textAlign: 'center', cursor: 'pointer' }}>
+            <PhoneOutlined
+              style={{
+                fontSize: 25,
+              }}
+            />
+            <div>语音聊天</div>
+          </div>
+          <div style={{ paddingLeft: 50, textAlign: 'center', cursor: 'pointer' }}>
+            <FontAwesomeIcon
+              icon={faVideo}
+              style={{
+                fontSize: 25,
+              }}
+            />
+            <div>视频聊天</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+UserProfile.propTypes = {
+  avatarUrl: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  status: PropTypes.oneOf(['online', 'offline']),
+  onCall: PropTypes.func,
+  onMessage: PropTypes.func,
+  onVideo: PropTypes.func,
+};
+
+UserProfile.defaultProps = {
+  status: 'offline', // 默认用户状态为离线
+  onCall: () => {},
+  onMessage: () => {},
+  onVideo: () => {},
+};
+
+export default UserProfile;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,24 +1,33 @@
-import React, {useEffect, useRef, useState} from 'react';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { AvatarDropdown } from '@/components/RightContent/AvatarDropdown';
+import {
+  listMessageVoByPageUsingPost,
+  listRoomVoByPageUsingPost,
+} from '@/services/backend/chatController';
+import { useModel } from '@@/exports';
+import { CodepenOutlined, MessageOutlined, UserOutlined } from '@ant-design/icons';
 import {
   Avatar,
   ChatContainer,
-  Conversation, ConversationHeader,
-  ConversationList, InfoButton,
-  MainContainer, Message, MessageInput, MessageList, MessageSeparator,
+  Conversation,
+  ConversationHeader,
+  ConversationList,
+  InfoButton,
+  MainContainer,
+  Message,
+  MessageInput,
+  MessageList,
+  MessageSeparator,
   Search,
-  Sidebar
-} from "@chatscope/chat-ui-kit-react";
-import {Empty, Tabs} from "antd";
-import {listMessageVoByPageUsingPost, listRoomVoByPageUsingPost} from "@/services/backend/chatController";
+  Sidebar,
+} from '@chatscope/chat-ui-kit-react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { Card, Col, Collapse, Divider, Empty, Row, Tabs } from 'antd';
 import moment from 'moment';
-import {useModel} from "@@/exports";
-import {CodepenOutlined, MessageOutlined, UserOutlined} from '@ant-design/icons';
-import {AvatarDropdown} from "@/components/RightContent/AvatarDropdown";
-
+import React, { useEffect, useRef, useState } from 'react';
+import UserProfile from './User/components/UserProfile';
 
 const Welcome: React.FC = () => {
-  const {initialState} = useModel('@@initialState');
+  const { initialState } = useModel('@@initialState');
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const [loadingMore, setLoadingMore] = useState(false);
   const [msgPageNum, setMsgPageNum] = useState(0);
@@ -26,12 +35,12 @@ const Welcome: React.FC = () => {
   const [msgTotal, setMsgTotal] = useState(0);
   const [messages, setMessages] = useState<API.ChatMessageResp[]>([]);
   const msgListRef = useRef(null);
-  const {currentUser} = initialState || {};
+  const { currentUser } = initialState || {};
   const [activeConversation, setActiveConversation] = useState<API.RoomVo>();
   const [conversations, setConversations] = useState<API.RoomVo[]>([]);
   // Set initial message input value to empty string
-  const [messageInputValue, setMessageInputValue] = useState("");
-  const [total, setTotal] = useState<string>()
+  const [messageInputValue, setMessageInputValue] = useState('');
+  const [total, setTotal] = useState<string>();
 
   //首先获取所有conversation
   useEffect(() => {
@@ -43,7 +52,7 @@ const Welcome: React.FC = () => {
     return () => {
       newSocket.close();
     };
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (socket && currentUser) {
@@ -59,30 +68,28 @@ const Welcome: React.FC = () => {
         setInterval(() => {
           if (socket.readyState === WebSocket.OPEN) {
             // 发送心跳消息，可以是任何你希望的消息，比如 'ping'
-            socket.send(JSON.stringify({type: 4}));
+            socket.send(JSON.stringify({ type: 4 }));
           }
         }, 10000); // 每10秒发送一次心跳消息
       };
       //接收数据
       socket.onmessage = (event) => {
-
         const res: API.Chat = JSON.parse(event.data);
         if (res.data && res.type === 2) {
           let target: API.RoomVo | null = null;
           const others: API.RoomVo[] = [];
-          const {roomId} = res.data;
+          const { roomId } = res.data;
           if (activeConversation && roomId === Number(activeConversation.id)) {
             //设置聊天消息
-            setMessages([...messages, res.data])
+            setMessages([...messages, res.data]);
             //设置左侧栏的消息
             for (const conversation of conversations) {
               if (Number(conversation.id) === roomId) {
-                target = {...conversation, content: res.data.message.content};
+                target = { ...conversation, content: res.data.message.content };
               } else {
                 others.push(conversation);
               }
             }
-
           }
           //消息来自其他聊天窗口（或者不属于任何一个聊天窗口）
           else {
@@ -91,14 +98,14 @@ const Welcome: React.FC = () => {
                 target = {
                   ...conversation,
                   unreadNum: Number(conversation.unreadNum) + 1,
-                  content: res.data.message.content
+                  content: res.data.message.content,
                 };
               } else {
                 others.push(conversation);
               }
             }
             if (!target) {
-              const {fromUser, message} = res.data;
+              const { fromUser, message } = res.data;
               target = {
                 id: roomId,
                 userId: fromUser.userId,
@@ -106,14 +113,13 @@ const Welcome: React.FC = () => {
                 avatar: fromUser.avatar,
                 unreadNum: 1,
                 content: message.content,
-                activeTime: message.activeTime
-              }
+                activeTime: message.activeTime,
+              };
             }
           }
           setConversations([target as API.RoomVo, ...others]);
         }
-
-      }
+      };
       //关闭连接
       socket.onclose = () => {
         console.log('WebSocket连接已关闭');
@@ -130,15 +136,15 @@ const Welcome: React.FC = () => {
     if (activeConversation && activeConversation.userId) {
       listMessageVoByPageUsingPost({
         pageSize: 30,
-        roomId: activeConversation.id
-      }).then(res => {
+        roomId: activeConversation.id,
+      }).then((res) => {
         if (res.code === 0) {
           if (res.data?.records) {
             setMessages(res.data?.records);
             setMsgTotal(Number(res.data?.total));
           }
         }
-      })
+      });
     }
   }, [activeConversation]);
 
@@ -148,8 +154,7 @@ const Welcome: React.FC = () => {
       const command = {
         type: 2,
         userId: activeConversation?.userId,
-        data: message
-
+        data: message,
       };
       socket.send(JSON.stringify(command));
     }
@@ -158,13 +163,18 @@ const Welcome: React.FC = () => {
     if (!activeConversation || newConversation.id !== activeConversation.id) {
       setInitLoaded(false);
       setMsgPageNum(0);
-      setActiveConversation({...newConversation, unreadNum: 0});
-      setConversations(conversations.map(conversation =>
-        Number(conversation.id) === Number(newConversation.id) ? {...newConversation, unreadNum: 0} : conversation));
+      setActiveConversation({ ...newConversation, unreadNum: 0 });
+      setConversations(
+        conversations.map((conversation) =>
+          Number(conversation.id) === Number(newConversation.id)
+            ? { ...newConversation, unreadNum: 0 }
+            : conversation,
+        ),
+      );
     }
     // @ts-ignore
     msgListRef.current?.scrollToBottom();
-  }
+  };
   // 组件加载时和页码变化时触发数据获取
   useEffect(() => {
     listRoomVoByPageUsingPost({
@@ -190,137 +200,197 @@ const Welcome: React.FC = () => {
       listMessageVoByPageUsingPost({
         current: msgPageNum,
         pageSize: 10,
-        roomId: activeConversation.id
-      }).then(res => {
+        roomId: activeConversation.id,
+      }).then((res) => {
         if (res.code === 0) {
           if (res.data?.records) {
             setMessages([...res.data?.records, ...messages]);
             setLoadingMore(false);
           }
         }
-      })
+      });
     }
   };
   return (
-
-    <Tabs tabPosition={"left"} style={{height: "100vh"}} indicator={{align: "center"}}
-          defaultActiveKey="1" tabBarExtraContent={<AvatarDropdown/>}>
-      <Tabs.TabPane style={{height: "100vh", paddingLeft: "0"}}
-                    icon={<MessageOutlined style={{fontSize: '26px'}}/>} key="1">
+    <Tabs
+      tabPosition={'left'}
+      style={{ height: '100vh' }}
+      indicator={{ align: 'center' }}
+      defaultActiveKey="1"
+      tabBarExtraContent={<AvatarDropdown />}
+    >
+      <Tabs.TabPane
+        style={{ height: '100vh', paddingLeft: '0' }}
+        icon={<MessageOutlined style={{ fontSize: '26px' }} />}
+        key="1"
+      >
         <MainContainer responsive>
-          <Sidebar style={{borderColor: "#DEDDDC"}} position="left" scrollable={false}>
-            <Search placeholder="Search..."/>
+          <Sidebar style={{ borderColor: '#DEDDDC' }} position="left" scrollable={false}>
+            <Search placeholder="Search..." />
             <ConversationList>
-              {
-                conversations.map(conversation =>
-                  <Conversation
-                    active={activeConversation && activeConversation.id === conversation.id}
-                    onClick={() => changeConversation(conversation)}
-                    lastActivityTime={moment(conversation.activeTime).format('MMMDo a h:mm ')}
-                    key={conversation.id}
-                    name={conversation.roomName}
-                    info={conversation.content}
-                    unreadCnt={conversation.unreadNum}
-                  >{
-                    conversation.type === 1 ? <Avatar src={conversation.avatar}/> :
-                      <Avatar src={conversation.avatar} status="available"/>
-                  }
-
-                  </Conversation>)
-              }
+              {conversations.map((conversation) => (
+                <Conversation
+                  active={activeConversation && activeConversation.id === conversation.id}
+                  onClick={() => changeConversation(conversation)}
+                  lastActivityTime={moment(conversation.activeTime).format('MMMDo a h:mm ')}
+                  key={conversation.id}
+                  name={conversation.roomName}
+                  info={conversation.content}
+                  unreadCnt={conversation.unreadNum}
+                >
+                  {conversation.type === 1 ? (
+                    <Avatar src={conversation.avatar} />
+                  ) : (
+                    <Avatar src={conversation.avatar} status="available" />
+                  )}
+                </Conversation>
+              ))}
             </ConversationList>
           </Sidebar>
-          {!activeConversation ?
-            <div style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-              <div style={{
-                backgroundImage: 'url(/images/empty.png)',
-                backgroundSize: '402px 204px',
-                marginBottom: 32,
-                width: 402,
-                height: 204
-              }}/>
-              <div style={{color: ' #8896b8', fontSize: 14, lineHeight: '20em'}}>
-                <Empty description={"快找小伙伴聊天吧 ( ゜- ゜)つロ"}/>.
+          {!activeConversation ? (
+            <div
+              style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <div
+                style={{
+                  backgroundImage: 'url(/images/empty.png)',
+                  backgroundSize: '402px 204px',
+                  marginBottom: 32,
+                  width: 402,
+                  height: 204,
+                }}
+              />
+              <div style={{ color: ' #8896b8', fontSize: 14, lineHeight: '20em' }}>
+                <Empty description={'快找小伙伴聊天吧 ( ゜- ゜)つロ'} />.
               </div>
-            </div> :
+            </div>
+          ) : (
             <ChatContainer>
               <ConversationHeader>
-                <ConversationHeader.Back/>
-                <Avatar
-                  src={activeConversation.avatar}
-                  name={activeConversation.id}/>
-                <ConversationHeader.Content userName={activeConversation.roomName}
-                                            info={moment(activeConversation.activeTime).format('MMMDo a h:mm ')}/>
+                <ConversationHeader.Back />
+                <Avatar src={activeConversation.avatar} name={activeConversation.id} />
+                <ConversationHeader.Content
+                  userName={activeConversation.roomName}
+                  info={moment(activeConversation.activeTime).format('MMMDo a h:mm ')}
+                />
                 <ConversationHeader.Actions>
-                  <InfoButton/>
+                  <InfoButton />
                 </ConversationHeader.Actions>
               </ConversationHeader>
               <MessageList ref={msgListRef} loadingMore={loadingMore} onYReachStart={onYReachStart}>
-                <MessageSeparator content={"上次的聊天"}/>
-                {
-                  messages.map((message, index) => {
-                    const flag = message.fromUser?.uid === currentUser?.id;
-                    return <Message style={{padding: 10}} key={index} model={{
-                      message: message.message?.content,
-                      sender: message.fromUser?.username,
-                      direction: flag ? "outgoing" : "incoming",
-                      position: "single"
-                    }} avatarPosition={flag ? 'tr' : 'tl'}>
-                      {activeConversation.type === 1 ? <Message.Header sender={message.fromUser?.username}/> : ""}
-                      <Avatar style={{width: 36, minWidth: 36, height: 36, minHeight: 36}}
-                              src={message.fromUser?.avatar}
-                              name={message.fromUser?.username}/>
+                <MessageSeparator content={'上次的聊天'} />
+                {messages.map((message, index) => {
+                  const flag = message.fromUser?.uid === currentUser?.id;
+                  return (
+                    <Message
+                      style={{ padding: 10 }}
+                      key={index}
+                      model={{
+                        message: message.message?.content,
+                        sender: message.fromUser?.username,
+                        direction: flag ? 'outgoing' : 'incoming',
+                        position: 'single',
+                      }}
+                      avatarPosition={flag ? 'tr' : 'tl'}
+                    >
+                      {activeConversation.type === 1 ? (
+                        <Message.Header sender={message.fromUser?.username} />
+                      ) : (
+                        ''
+                      )}
+                      <Avatar
+                        style={{ width: 36, minWidth: 36, height: 36, minHeight: 36 }}
+                        src={message.fromUser?.avatar}
+                        name={message.fromUser?.username}
+                      />
                     </Message>
-                  })
-                }
+                  );
+                })}
               </MessageList>
-              <MessageInput placeholder="Type message here" value={messageInputValue}
-                            onChange={val => setMessageInputValue(val)}
-                            onSend={() => {
-                              setMessageInputValue("")
-                              const msg = {
-                                type: activeConversation?.type,
-                                toUid: activeConversation?.userId,
-                                content: messageInputValue,
-                              }
-                              const msgVo: API.ChatMessageResp = {
-                                fromUser: {
-                                  uid: currentUser?.id,
-                                  username: currentUser?.userName,
-                                  avatar: currentUser?.userAvatar,
-                                },
-                                message: {
-                                  content: messageInputValue,
-                                },
-                              }
-                              setMessages([...messages, msgVo]);
-                              setConversations([{...activeConversation, content: messageInputValue},
-                                ...conversations.filter(conversation =>
-                                  conversation.id !== activeConversation?.id)])
-                              sendMessage(msg);
-                            }}/>
+              <MessageInput
+                placeholder="Type message here"
+                value={messageInputValue}
+                onChange={(val) => setMessageInputValue(val)}
+                onSend={() => {
+                  setMessageInputValue('');
+                  const msg = {
+                    type: activeConversation?.type,
+                    toUid: activeConversation?.userId,
+                    content: messageInputValue,
+                  };
+                  const msgVo: API.ChatMessageResp = {
+                    fromUser: {
+                      uid: currentUser?.id,
+                      username: currentUser?.userName,
+                      avatar: currentUser?.userAvatar,
+                    },
+                    message: {
+                      content: messageInputValue,
+                    },
+                  };
+                  setMessages([...messages, msgVo]);
+                  setConversations([
+                    { ...activeConversation, content: messageInputValue },
+                    ...conversations.filter(
+                      (conversation) => conversation.id !== activeConversation?.id,
+                    ),
+                  ]);
+                  sendMessage(msg);
+                }}
+              />
             </ChatContainer>
-          }
+          )}
         </MainContainer>
       </Tabs.TabPane>
-      <Tabs.TabPane icon={<UserOutlined style={{fontSize: '26px'}}/>} style={{height: "100vh", paddingLeft: "0"}}
-                    key="2">
-        Content of Tab Pane 2
+      <Tabs.TabPane
+        icon={<UserOutlined style={{ fontSize: '26px' }} />}
+        style={{ height: '100vh', paddingLeft: '0' }}
+        key="2"
+      >
+        <Row>
+          <Col span={6} style={{ paddingLeft: 10 }}>
+            <Collapse>
+              <Collapse.Panel key={'1'} header={'好友'}>
+                111
+              </Collapse.Panel>
+              <Collapse.Panel key={'2'} header={'群聊'}>
+                222
+              </Collapse.Panel>
+            </Collapse>
+          </Col>
+          <Divider type="vertical" style={{ height: '100vh' }} />
+          <Col span={16} style={{ padding: 80 }}>
+            <Card>
+              <div style={{ padding: 120 }}>
+                <UserProfile
+                  avatarUrl="https://img0.baidu.com/it/u=3289461667,2295508139&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1709398800&t=edce1aab1fde64d9a4d59ab7f3cff730"
+                  userName="Likty_1225"
+                  status="online"
+                  onCall={() => console.log('拨打电话')}
+                  onMessage={() => console.log('发送信息')}
+                  onVideo={() => console.log('开始视频通话')}
+                />
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </Tabs.TabPane>
-      <Tabs.TabPane icon={<CodepenOutlined style={{fontSize: '26px'}}/>} style={{height: "100vh", paddingLeft: "0"}}
-                    key="3">
+      <Tabs.TabPane
+        icon={<CodepenOutlined style={{ fontSize: '26px' }} />}
+        style={{ height: '100vh', paddingLeft: '0' }}
+        key="3"
+      >
         Content of Tab Pane 3
       </Tabs.TabPane>
     </Tabs>
-
-  )
+  );
 };
 
 export default Welcome;
